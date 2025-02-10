bbadbec5f95f9800795441da8daf48c0dda9ac3e
Justin Leonardo <J.leonardo11n@gmail.com>
2025-02-10 15:38:56 -0800

    05 got me confused with the unknown property

--- dist/ex08.js
+++ dist/ex08.js
@@ -1,25 +1,14 @@
+"use strict";
 // Exercise 8: Optional Chaining
 // Create an interface for a "House" object with a nested "address" object that includes an optional "city" (string).
 // Use optional chaining to safely access the "city" and log it to the console.
-
-interface Address {
-  city?:string;
-
-}
-
-interface House{
-address?:Address;
-}
-
 const myHouse = {
-  address: {
-    city: "New York"
-  }
+    address: {
+        city: "New York"
+    }
 };
-
-console.log( );
+console.log();
 // Expected Output: "New York"
-
 const noCityHouse = {};
-console.log( );
+console.log();
 // Expected Output: "City not available"
